645affb1ed996cbd489272821a2a1e8f8e355ed3
vercetti322 <vercettitommy322@gmail.com>
2026-03-22 11:54:43 +0530

    project init!

---
+++ src/main/java/com/app/hikaribase/HikaribaseApplication.java
@@ -0,0 +1,12 @@
+package com.app.hikaribase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikaribaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikaribaseApplication.class, args);
+	}
+}
